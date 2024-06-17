cb7e1ba071653e28c060fcff343a49255ace2e07
Emily Curcio <166759445+emilycurcio@users.noreply.github.com>
2024-06-17 16:08:17 -0700

    Revisions

--- Dashboards_Streamlit_Part2.py
+++ Dashboards_Streamlit_Part2.py
@@ -36,8 +36,15 @@ df = pd.read_csv('subset_new_york_data.csv', index_col = 0)
 ### INTRO PAGE
 
 if page == 'Intro page':
-    st.markdown('#### This dashboard aims to provide helpful insights on the expansion problems NY CitiBikes currently faces.')
-    st.markdown('Right now, CitiBikes runs into a situation where customers complain about bikes not being available at certain times. This analysis will look at the potential reasons behind this. The dashboard is separated into four sections:')
+    st.markdown('This dashboard aims to provide helpful insights on the expansion problems NY CitiBikes currently faces. Right now, CitiBikes runs into a situation where customers complain about bikes not being available at certain times. This analysis will look at the potential reasons behind this.')
+    st.markdown('#### Overall Approach:')
+    st.markdown('1. Define Objective')
+    st.markdown('2. Source Data')
+    st.markdown('3. Geospatial Plot')
+    st.markdown('4. Interactive Visualizations')
+    st.markdown('5. Dashboard Creation')
+    st.markdown('6. Findings and Recommendations')
+    st.markdown('#### Dashboard Sections:')
     st.markdown('- Weather component and bike usage')
     st.markdown('- Most popular stations')
     st.markdown('- Interactive map with aggregated bike trips')
@@ -47,6 +54,7 @@ if page == 'Intro page':
 
     myImage = Image.open('CitiBikes.jpg') # Source: https://www.nydailynews.com/2019/07/10/citi-bike-neglects-poor-nyc-neighborhoods-and-communities-of-color-report/
     st.image(myImage)
+    st.markdown('Source: https://www.nydailynews.com/2019/07/10/citi-bike-neglects-poor-nyc-neighborhoods-and-communities-of-color-report')
 
 
 ### LINE CHART PAGE: WEATHER COMPONENT AND BIKE USAGE
@@ -69,7 +77,7 @@ elif page == 'Weather component and bike usage':
             y = df_aggregated['bike_rides_daily'],
             name = 'Daily Bike Rides',
             yaxis = 'y1',  # Specify primary y-axis
-            line = dict(color = 'green')  # Set the color for bike rides
+            line = dict(color = 'blue')  # Set the color for bike rides
         )
     )
 
@@ -90,8 +98,8 @@ elif page == 'Weather component and bike usage':
         xaxis_title = 'Date',
         yaxis = dict(
             title = 'Daily Total Bike Rides',
-            titlefont = dict(color = 'green'),
-            tickfont = dict(color = 'green')
+            titlefont = dict(color = 'blue'),
+            tickfont = dict(color = 'blue')
         ),
         yaxis2 = dict(
             title = 'Daily Avg Temperature',
@@ -133,7 +141,7 @@ elif page == 'Most popular stations':
     df_groupby_bar = df1.groupby('start_station_name', as_index = False).agg({'value': 'sum'})
     top20 = df_groupby_bar.nlargest(20, 'value')
 
-    fig = go.Figure(go.Bar(x = top20['start_station_name'], y = top20['value'], marker = {'color': top20['value'], 'colorscale': 'Greens'}))
+    fig = go.Figure(go.Bar(x = top20['start_station_name'], y = top20['value'], marker = {'color': top20['value'], 'colorscale': 'Blues'}))
 
     fig.update_layout(
         title = 'Top 20 Most Popular Bike Stations in NYC 2022',
@@ -173,8 +181,8 @@ elif page == 'Classic versus electric bikes':
     electric_bike = df[df['rideable_type'] == 'electric_bike']
 
     # Create histograms for each category
-    hist_data_A = go.Histogram(x = classic_bike['avgTemp'], name = 'Classic Bike', marker = dict(color = 'green'))
-    hist_data_B = go.Histogram(x = electric_bike['avgTemp'], name = 'Electric Bike', marker = dict(color = 'lightgreen'))
+    hist_data_A = go.Histogram(x = classic_bike['avgTemp'], name = 'Classic Bike', marker = dict(color = 'blue'))
+    hist_data_B = go.Histogram(x = electric_bike['avgTemp'], name = 'Electric Bike', marker = dict(color = 'lightblue'))
 
     # Create layout
     layout = go.Layout(
@@ -201,6 +209,7 @@ else:
     st.header('Conclusions and Recommendations')
     bikes = Image.open('CitiBike.jpg')  # Source: https://nycdotbikeshare.info/news-and-events/citi-bike-launch-nyc
     st.image(bikes)
+    st.markdown('Source: https://nycdotbikeshare.info/news-and-events/citi-bike-launch-nyc')
     st.markdown('### Our analysis has shown that NY CitiBikes should focus on the following objectives moving forward:')
     st.markdown('- There is a clear correlation between temperature and bike trips. We recommend ensuring that stations are fully stocked during the warmer months in order to meet the higher demand, but to provide a lower supply in winter and late autumn to reduce cost.')
     st.markdown('- There is a clear popularity among the stations along the water and around Central Park. We recommend adding bikes and bike parking to these locations.')
